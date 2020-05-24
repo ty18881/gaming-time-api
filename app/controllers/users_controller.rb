0923f747dac13f336a9145e65dc3eca69ec19605
ty18881 <ty18881@gmail.com>
2020-05-24 13:49:50 -0400

    User Controller - collect and save parent information when creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,29 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    # @user = User.new(user_params)
+    @user = User.new
+
+    @user.name = params[:user][:name]
+    @user.age = params[:user][:age]
+    @user.daily_goal = 30
+    @user.time_eligible=false
 
     if @user.save
-      render json: @user, status: :created, location: @user
+
+      # successfully saved the user, now save the companion record
+
+      @parent = Parent.new
+      @parent.name = params[:user][:parentName]
+      @parent.email = params[:user][:parentEmail]
+      @parent.user_id = @user.id
+
+      if @parent.save
+        render json: @parent, status: :created, location: @parent
+        # render json: @user, status: :created, location: @user
+      else
+        render json: @parent.errors, status: :unprocessable_entity
+      end
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -46,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :age, :daily_goal, :time_eligible)
+      params.require(:user).permit(:name, :age, :daily_goal, :time_eligible, :parentName, :parentEmail)
     end
 end
